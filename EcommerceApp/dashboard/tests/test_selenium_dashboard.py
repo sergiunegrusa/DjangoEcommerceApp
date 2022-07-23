29e5e3df4523861cf56190ac98bfa6ce3a240436
sergiunegrusa <sergiunegrusa@yahoo.com>
2022-07-24 01:35:42 +0300

    pytest-django added

---
+++ EcommerceApp/dashboard/tests/test_selenium_dashboard.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium.webdriver.common.keys import Keys
+
+def test_example():
+    assert "Hello" == "Hello"
+
+
+@pytest.mark.selenium
+def test_dashboard_admin_login():
+    
+
+@pytest.mark.selenium
+def ....
