62868e1020ba42241d53d60d474a5e4e69290d9a
sergiunegrusa <sergiunegrusa@yahoo.com>
2022-07-25 17:06:52 +0300

    prdouct factory many to many connection

--- EcommerceApp/demo/management/commands/load-fixtures.py
+++ EcommerceApp/demo/management/commands/load-fixtures.py
@@ -6,6 +6,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
             call_command("makemigrations")
             call_command("migrate")
-            
+
             call_command("loaddata", "db_admin_fixture.json")
             call_command("loaddata", "db_category_fixture.json")
+            call_command("loaddata", "db_product_fixture.json")
