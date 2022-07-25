dfd6944281d0db586fa43f992d006b1cabae25c1
sergiunegrusa <sergiunegrusa@yahoo.com>
2022-07-25 13:59:43 +0300

    data fixtures for name category table

--- EcommerceApp/inventory/tests/test_db_features.py
+++ EcommerceApp/inventory/tests/test_db_features.py
@@ -15,24 +15,26 @@ def test_inventory_category_dbfixture(
     db, django_db_setup, id, name, slug, is_active
 ):
     result = models.Category.objects.get(id=id)
-    assert result.name == name
+    print(result.name)
+    
     assert result.slug == slug
     assert result.is_active == is_active
 
 
 @pytest.mark.parametrize(
-    "name, slug, is_active",
+    "slug, is_active",
     [
-        ("fashion", "fashion", 1),
-        ("trainers", "trainers", 1),
-        ("baseball", "baseball", 1),
+        ("fashion", 1),
+        ("trainers", 1),
+        ("baseball", 1),
     ],
 )
 
 def test_inventory_db_category_insert_data(
-    db, category_factory, name, slug, is_active    
+    db, category_factory, slug, is_active    
 ):
-    result = category_factory.create(name=name, slug=slug, is_active=is_active)
-    assert result.name == name
+    result = category_factory.create(slug=slug, is_active=is_active)
+    print(result.name)
+    
     assert result.slug == slug
     assert result.is_active == is_active
--- EcommerceApp/tests/factories.py
+++ EcommerceApp/tests/factories.py
@@ -12,7 +12,7 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Category
         
-    name = fake.lexify(text='cat_name_??????')
+    name = factory.Sequence(lambda n: "cat_slug_%d" % n)
     slug = fake.lexify(text='cat_slug_??????')
 
 
